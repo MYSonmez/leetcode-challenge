ade8d3e9231e6490b649101f2e5426f5f1e0cbfe
Yusuf Sonmez <m.yusufsonmez03@gmail.com>
2024-09-28 19:24:47 +0100

    Add description for two sum

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -1,7 +1,14 @@
 package easy;
 
+/***
+ * QUESTION:
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ * 
+ * @author m.yusufsonmez03@gmail.com
+ */
 public class TwoSum {
-
     public int[] twoSum(int[] nums, int target) {
         int len = nums.length;
         for (int i = 0; i < len; i++) {
