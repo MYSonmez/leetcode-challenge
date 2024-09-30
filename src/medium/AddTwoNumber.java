8d7f3b2e121998967b35d04b09d1fad84ab2e3ae
Yusuf Sonmez <m.yusufsonmez03@gmail.com>
2024-09-30 13:35:25 +0100

    Added Question Description

--- src/medium/AddTwoNumber.java
+++ src/medium/AddTwoNumber.java
@@ -1,10 +1,10 @@
 package medium;
 
 import java.math.BigInteger;
-import java.util.List;
 
 /**
- * QUESTION? You are given two non-empty linked lists representing two
+ * QUESTION:
+ * You are given two non-empty linked lists representing two
  * non-negative integers. The digits are stored in reverse order, and each of
  * their nodes contains a single digit. Add the two numbers and return the sum
  * as a linked list. You may assume the two numbers do not contain any leading
@@ -14,39 +14,9 @@ import java.util.List;
  */
 public class AddTwoNumber {
 
-    public static void main(String[] args) {
-        var l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
-        var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
-
-        addTwoNumbers(l1, l2);
-
-    }
-
-    private static BigInteger CountTotal(ListNode node) {
-        BigInteger decimalPower = BigInteger.ONE;
-
-        BigInteger total = BigInteger.ZERO;
-        while (node != null) {
-            var ara = decimalPower.multiply(new BigInteger(String.valueOf(node.val)));
-            var deger = decimalPower.multiply(ara);
-            total.add(decimalPower.multiply(deger));
-            decimalPower.multiply(BigInteger.TEN);
-            node = node.next;
-        }
-
-        return total;
-    }
-
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        BigInteger total1 = CountTotal(l1);
-        BigInteger total2 = CountTotal(l2);
-
-        BigInteger total = total1.add(total2);
-
-        String totalAsString = String.valueOf(total);
-        int totalAsStringLen = totalAsString.length();
-        var charArr = totalAsString.toCharArray();
-
+        char[] charArr = CountTotal(l1).add(CountTotal(l2)).toString().toCharArray();
+        int totalAsStringLen = charArr.length;
         ListNode l3 = new ListNode(Integer.parseInt(Character.toString(charArr[totalAsStringLen - 1])));
         ListNode temp = l3;
         for (int i = totalAsStringLen - 2; i >= 0; i--) {
@@ -54,6 +24,18 @@ public class AddTwoNumber {
             l3 = l3.next;
         }
         return temp;
+    }
+
+    private static BigInteger CountTotal(ListNode node) {
+        BigInteger decimalPower = BigInteger.ONE;
+        BigInteger total = BigInteger.ZERO;
+        while (node != null) {
+            total = total.add(decimalPower.multiply(new BigInteger(String.valueOf(node.val))));
+            decimalPower = decimalPower.multiply(BigInteger.TEN);
+            node = node.next;
+        }
+
+        return total;
     }
 
 }
